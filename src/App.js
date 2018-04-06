61e7b48e5ff4aa9a0c618af78e1d361dc0dc45cc
James Kamana <jaykamana@gmail.com>
2018-04-06 11:38:39 +0300

    Add component interface with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
 import "./App.css";
 
 const DEFAULT_QUERY = "redux";
@@ -11,11 +12,21 @@ const PARAM_SEARCH = "query=";
 const PARAM_PAGE = "page=";
 const PARAM_HPP = "hitsPerPage=";
 
-const Button = ({ onClick, className = "", children }) => (
+const Button = ({ onClick, className, children }) => (
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
 );
+
+Button.defaultProps = {
+  className: ""
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
 
 const Search = ({ value, onChange, onSubmit, children }) => (
   <form onSubmit={onSubmit}>
@@ -23,6 +34,11 @@ const Search = ({ value, onChange, onSubmit, children }) => (
     <button type="submit">{children}</button>
   </form>
 );
+Search.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
 
 const Table = ({ list, pattern, onDismiss }) => (
   <div className="table">
@@ -46,6 +62,19 @@ const Table = ({ list, pattern, onDismiss }) => (
     ))}
   </div>
 );
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
 
 class App extends Component {
   _isMounted = false;
